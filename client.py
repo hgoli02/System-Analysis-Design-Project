f33cb63288a3837ee0b9d02389ff746289f4af78
hgoli02 <79108091+hgoli02@users.noreply.github.com>
2023-12-22 12:14:27 +0330

    multithreaded server + subscribe

--- client.py
+++ client.py
@@ -1,17 +1,19 @@
 import socket
-
+import threading
+from threading import Thread
+import time
 
 def init():
     # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect to the server
+    server_address = ('127.0.0.1', 8888)
+    client_socket.connect(server_address)
 
     return client_socket
 
 def push(client_socket, message):
-    client_socket.send(message.encode())
+    client_socket.send(message.encode('utf-8'))
     data = client_socket.recv(1024).decode()
     print('Received from server: ' + data)
     return data
@@ -22,8 +24,29 @@ def pull(client_socket):
     print('Received from server: ' + data)
     return data
 
-def subscribe(client_socket, topic):
-    pass
+def subscribe_runner(host, port):
+    sub_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sub_socket.connect((host, port))
+    print("Connected to server")
+    while True:
+        sub_socket.send("subscribe".encode())
+        data = sub_socket.recv(1024).decode()
+        if data == "No messages":
+            time.sleep(1)
+        else:
+            print(data)
+
+def subscribe(host, port):
+    #create a Thread
+    #subscribe to server and listen for messages
+    #print messages as they come in
+    #if message is "quit", then close connection
+    #Create a new Thread (just do it)
+
+    thread = Thread(target = subscribe_runner, args = (host, port,))
+    thread.start()
+
+    
 
 if __name__ == '__main__':
     client_socket = init()
@@ -31,6 +54,8 @@ if __name__ == '__main__':
         data = input(' -> ')
         if data == "get":
             pull(client_socket)
+        elif data == "subscribe":
+            subscribe('127.0.0.1', 8888)
         else:
             push(client_socket, data)
     client_socket.close()  # close the connection
--- server.py
+++ server.py
@@ -1,39 +1,49 @@
 import socket
+import threading
+import time
+
 queue = []
 
-#I am writing a message broker server
-
-def server_program():
-    # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
-
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-    server_socket.listen(5)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
+def handle_client(client_socket):
+    # Handle the client's request
     while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        if data == "get":
-            if len(queue) == 0:
-                conn.send("No messages in queue".encode())
-            else:
-                conn.send(queue.pop(0).encode())
+        request = client_socket.recv(1024)
+        print(f"Received: {request.decode('utf-8')}")
+        if request.decode('utf-8') == "get":
+            response = queue.pop(0)
+            client_socket.send(response.encode('utf-8'))
+        elif request.decode('utf-8') == "subscribe":
+            if len(queue) <= 0:
+                client_socket.send("No messages".encode('utf-8'))
+                time.sleep(1)
+                continue
+            response = queue.pop(0)
+            client_socket.send(response.encode('utf-8'))
         else:
-            queue.append(data)
-            conn.send("Message added to queue".encode())
-        print("from connected user: " + str(data))
-        #data = input(' -> ')
-        #conn.send(data.encode())  # send data to the client
+            queue.append(request.decode('utf-8'))
+            client_socket.send("OK".encode('utf-8'))
 
-    conn.close()  # close the connection
+    #client_socket.close()
 
+def start_server():
+    # Create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if __name__ == '__main__':
-    server_program()
+    # Bind the socket to a specific address and port
+    server_socket.bind(('127.0.0.1', 8888))
+
+    # Listen for incoming connections
+    server_socket.listen(5)
+    print("Server listening on port 8888")
+
+    while True:
+        # Accept a connection from a client
+        client_socket, addr = server_socket.accept()
+        print(f"Accepted connection from {addr}")
+
+        # Create a new thread to handle the client
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
+
+if __name__ == "__main__":
+    start_server()
