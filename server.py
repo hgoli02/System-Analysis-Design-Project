8b218513ca77b88fa66e1f1c6ced164bc4c7d067
kmykz <kamyarkazari@gmail.com>
2023-12-26 15:30:54 +0330

    The Queue implementation using file is done.

--- server.py
+++ server.py
@@ -2,15 +2,44 @@ import socket
 import threading
 import time
 
-queue = []
+queue_address = 'DB/queue.txt'
+#A class for handling the queue through a file
+class Queue:
+    def __init__(self, queue_address):
+        self.queue_address = queue_address
+        self.queue = []
+        self.load_queue()
 
+    def load_queue(self):
+        with open(self.queue_address, 'r') as f:
+            for line in f:
+                self.queue.append(line)
+
+    def save_queue(self):
+        with open(self.queue_address, 'w') as f:
+            for line in self.queue:
+                f.write(line+"\n")
+
+    def push(self, item):
+        self.queue.append(item)
+        self.save_queue()
+
+    def pop(self):
+        item = self.queue.pop(0)
+        self.save_queue()
+        return item
+
+    def __len__(self):
+        return len(self.queue)
+
+queue = Queue(queue_address)
 def handle_client(client_socket):
     # Handle the client's request
     while True:
         request = client_socket.recv(1024)
         print(f"Received: {request.decode('utf-8')}")
         if request.decode('utf-8') == "get":
-            response = queue.pop(0)
+            response = "No messages" if len(queue) <= 0 else queue.pop()
             client_socket.send(response.encode('utf-8'))
         elif request.decode('utf-8') == "subscribe":
             if len(queue) <= 0:
@@ -20,7 +49,7 @@ def handle_client(client_socket):
             response = queue.pop(0)
             client_socket.send(response.encode('utf-8'))
         else:
-            queue.append(request.decode('utf-8'))
+            queue.push(request.decode('utf-8'))
             client_socket.send("OK".encode('utf-8'))
 
     #client_socket.close()
