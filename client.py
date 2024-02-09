174164be77780ddfc1176dd054b9812f2b9a2cca
hgoli02 <79108091+hgoli02@users.noreply.github.com>
2024-02-09 11:37:15 +0330

    Added tests

--- client.py
+++ client.py
@@ -1,61 +1,32 @@
-import socket
-import threading
-from threading import Thread
+import requests
 import time
+from threading import Thread
+import argparse
 
-def init():
-    # get the hostname
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Connect to the server
-    server_address = ('127.0.0.1', 8888)
-    client_socket.connect(server_address)
+class PyClient:
+    def __init__(self, server_url='localhost', port=8000):
+        self.server_url = f'http://127.0.0.1:{port}' if server_url == 'localhost' else server_url
 
-    return client_socket
+    def push(self, key, value):
+        message = key+','+value
+        response = requests.post(self.server_url + '/push', data=message)
+        print('Received from server: ' + response.text)
+        return response.text
 
-def push(client_socket, message):
-    client_socket.send(message.encode('utf-8'))
-    data = client_socket.recv(1024).decode()
-    print('Received from server: ' + data)
-    return data
+    def pull(self):
+        response = requests.get(self.server_url + '/pull')
+        print('Received from server: ' + response.text)
+        return response.text
 
-def pull(client_socket):
-    client_socket.send("get".encode())
-    data = client_socket.recv(1024).decode()
-    print('Received from server: ' + data)
-    return data
+    def subscribe_runner(self, url):
+        while True:
+            response = requests.get(url + '/pull')
+            data = response.text
+            if data == "no message":
+                time.sleep(1)
+            else:
+                print('Subscribed message: ' + data)
 
-def subscribe_runner(host, port):
-    sub_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sub_socket.connect((host, port))
-    print("Connected to server")
-    while True:
-        sub_socket.send("subscribe".encode())
-        data = sub_socket.recv(1024).decode()
-        if data == "No messages":
-            time.sleep(1)
-        else:
-            print(data)
-
-def subscribe(host, port):
-    #create a Thread
-    #subscribe to server and listen for messages
-    #print messages as they come in
-    #if message is "quit", then close connection
-    #Create a new Thread (just do it)
-
-    thread = Thread(target = subscribe_runner, args = (host, port,))
-    thread.start()
-
-    
-
-if __name__ == '__main__':
-    client_socket = init()
-    while True:
-        data = input(' -> ')
-        if data == "get":
-            pull(client_socket)
-        elif data == "subscribe":
-            subscribe('127.0.0.1', 8888)
-        else:
-            push(client_socket, data)
-    client_socket.close()  # close the connection
+    def subscribe(self):
+        thread = Thread(target=self.subscribe_runner, args=(self.server_url,))
+        thread.start()
