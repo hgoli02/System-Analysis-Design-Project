b747332542334dcbf78c0c4b2c2e5caefea5d531
Kian Bahadori <79361265+Kian-bhd@users.noreply.github.com>
2024-02-10 21:50:35 +0330

    Create CI_test.py

---
+++ CI_test.py
@@ -0,0 +1,19 @@
+from client import PyClient as Client
+
+client = Client()
+
+for i in range(100):
+    client.push(f'{i}', f'{i}')
+
+results = []
+for i in range(100):
+    temp = client.pull()
+    results.append(temp)
+
+print(sorted(results))
+
+for i in range(100):
+    assert f'{i}' in results
+
+print('Test passed')
+
