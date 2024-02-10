7484ba2320ef7d54167986c55f8988e476f739c5
hgoli02 <79108091+hgoli02@users.noreply.github.com>
2024-02-10 22:36:56 +0330

    what the hell?

--- CI_test.py
+++ CI_test.py
@@ -2,15 +2,17 @@ from client import PyClient as Client
 import time
 
 client = Client()
-C = 10
+C = 20
 for i in range(C):
     client.push(f'{i}', f'{i}')
+    time.sleep(0.5)
 
 time.sleep(1)
 
 results = []
 for i in range(C):
     temp = client.pull()
+    thread.sleep(0.5)
     if temp == 'no message':
         results.append(-1)
     else:
