7a06a407b266e0eac3e538d6e45c87e6b7411bd8
hgoli02 <79108091+hgoli02@users.noreply.github.com>
2024-02-10 22:21:48 +0330

    a test

--- test.py
+++ test.py
@@ -2,15 +2,15 @@ from client import PyClient as Client
 import time
 
 client = Client()
-C = 50
+C = 5
 for i in range(C):
     client.push(f'{i}', f'{i}')
-    time.sleep(0.01)
+    time.sleep(0.05)
 
 results = []
 for i in range(C):
     temp = client.pull()
-    time.sleep(0.01)
+    time.sleep(0.05)
     if temp == 'no message':
         results.append(-1)
     else:
