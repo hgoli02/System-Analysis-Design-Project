2ce13613ae5d2a2d0053ac63ec97975b41aab2ff
hgoli02 <79108091+hgoli02@users.noreply.github.com>
2024-02-11 10:56:15 +0330

    Update server URL and port handling

--- client.py
+++ client.py
@@ -2,21 +2,29 @@ import requests
 import time
 from threading import Thread
 import argparse
+import random
 
 class PyClient:
     def __init__(self, server_url='localhost', port=8000, verbose=False):
-        self.server_url = f'http://127.0.0.1:{port}' if server_url == 'localhost' else server_url
+        self.server_url = f'http://127.0.0.1' if server_url == 'localhost' else server_url
         self.verbose = verbose
+        self.port = port
+
+    def get_url(self):
+        if (random.random() > 0.5):
+            return self.server_url + ':' + str(self.port)
+        else:
+            return self.server_url + ':' + str(self.port)
 
     def push(self, key, value):
         message = key+','+value
-        response = requests.post(self.server_url + '/push', data=message)
+        response = requests.post(self.get_url() + '/push', data=message)
         if self.verbose:
             print('Received from server: ' + response.text)
         return response.text
 
     def pull(self):
-        response = requests.get(self.server_url + '/pull')
+        response = requests.get(self.get_url() + '/pull')
         if self.verbose:
             print('Received from server: ' + response.text)
         return response.text
