260872cdf31e0c788f58ce01b48b8475e17f2e13
hgoli02 <79108091+hgoli02@users.noreply.github.com>
2024-09-14 17:13:29 +0330

    refactor the codes

--- scale_test.ps1
+++
@@ -1,8 +0,0 @@
-docker build . -t kalhasti:latest;  docker compose down;  docker compose up --scale queue=1 -d
-Start-Sleep -Milliseconds 1000
-python test.py
-Read-Host -Prompt "Press Enter to continue"
-docker-compose up --force-recreate --no-deps gateway -d
-docker compose up --scale queue=4 -d
-Start-Sleep -Milliseconds 1000
-python test.py
--- tests/scale_test.ps1
+++ tests/scale_test.ps1
@@ -1,8 +1,8 @@
-docker build . -t kalhasti:latest;  docker compose down;  docker compose up --scale queue=1
+docker build . -t kalhasti:latest;  docker compose down;  docker compose up --scale queue=1 -d
 Start-Sleep -Milliseconds 1000
-python scale_test1.py
+python test.py
 Read-Host -Prompt "Press Enter to continue"
-docker-compose up --force-recreate --no-deps gateway
-docker compose up --scale queue=4 
+docker-compose up --force-recreate --no-deps gateway -d
+docker compose up --scale queue=4 -d
 Start-Sleep -Milliseconds 1000
-python scale_test1.py
+python test.py
